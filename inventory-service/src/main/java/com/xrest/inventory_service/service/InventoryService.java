dda8305175f8ee1ec13f911bc7a49399733a4d87
Sonamxrest <xresthasonam@gmail.com>
2023-01-17 21:14:43 +0545

    Added Resiliance Curcit Breaker

--- inventory-service/src/main/java/com/xrest/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/xrest/inventory_service/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.xrest.inventory_service.service;
 import com.xrest.inventory_service.dto.InventoryResponse;
 import com.xrest.inventory_service.model.Inventory;
 import com.xrest.inventory_service.repository.InventoryRepository;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
@@ -19,7 +22,10 @@ public class InventoryService {
     }
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skucode) {
+       log.info("SLEEPPPP:::::");
+       Thread.sleep(10000);
         return inventoryRepository.findInventoriesBySkuCodeIn(skucode).stream().map(this::mapToDto).collect(Collectors.toList());
     }
 
--- order-service/src/main/java/com/xrest/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/xrest/order_service/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.xrest.order_service.controller;
 
 import com.xrest.order_service.dto.OrderRequestDto;
 import com.xrest.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/v1/order")
@@ -15,8 +20,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallBack")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
         orderService.placeOrder(orderRequestDto);
-        return "Order Placed Successfully";
+        return CompletableFuture.supplyAsync(() -> "Order Placed Successfully");
+    }
+
+    public CompletableFuture<String> fallBack(OrderRequestDto orderRequestDto,RuntimeException ex) {
+        return  CompletableFuture.supplyAsync(() ->"fallback called");
     }
 }
